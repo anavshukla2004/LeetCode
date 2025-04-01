6585f43b19df0eadaabaa077f7a3af267b7eb078
anavshukla2004 <160575822+anavshukla2004@users.noreply.github.com>
2025-04-01 11:33:38 +0530

    Time: 168 ms (5.02%) | Memory: 45.5 MB (10.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -7,7 +7,7 @@ class Solution {
             int len = 0;
 
             for (int j = i; j < s.length(); j++) {
-                if (sb.toString().contains(String.valueOf(s.charAt(j)))) { 
+                if (sb.indexOf(String.valueOf(s.charAt(j))) != -1) { 
                     break;
                 }
                 sb.append(s.charAt(j));
