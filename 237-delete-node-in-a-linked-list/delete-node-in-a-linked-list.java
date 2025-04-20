eb1ebdd4bdb873941f9a020634b0d792e54527fb
anavshukla2004 <160575822+anavshukla2004@users.noreply.github.com>
2025-04-20 11:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (65.98%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,9 +8,7 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-         if (node == null || node.next == null) {
-        throw new IllegalArgumentException("Can't delete the last node or null node this way.");
-    }
+         
 
     node.val = node.next.val;
     node.next = node.next.next;
