871d94b03860a7ac2b4a2cd1c6a205d36ffaceaa
anavshukla2004 <160575822+anavshukla2004@users.noreply.github.com>
2025-05-30 04:35:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (32.92%) - LeetSync

--- 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.java
+++ 3519-find-the-number-of-winning-players/find-the-number-of-winning-players.java
@@ -1,28 +1,28 @@
 class Solution {
     public int winningPlayerCount(int n, int[][] pick) {
-        int winners = 0;
-        Map<Integer, Map<Integer, Integer>> playerBallCount = new HashMap<>();
-        for (int[] p : pick) {
-            int player = p[0];
-            int color = p[1];
-            playerBallCount.putIfAbsent(player, new HashMap<>());
-            Map<Integer, Integer> colorCount = playerBallCount.get(player);
-            colorCount.put(color, colorCount.getOrDefault(color, 0) + 1);
-        }
+        
+        int[][] arr = new int[n][11];
 
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry : playerBallCount.entrySet()) {
-            int player = entry.getKey();
-            Map<Integer, Integer> colorCount = entry.getValue();
-            
-            for (int count : colorCount.values()) {
-                if (count > player) {
-                    winners++;
+        
+
+        for(int i=0;i<pick.length;i++){
+            int p = pick[i][0];
+            int ch = pick[i][1];
+
+            arr[p][ch]++;
+
+        }
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=10;j++){
+                if(arr[i][j]>=(i+1)){
+                    count++;
                     break;
                 }
             }
         }
 
-        return winners;
+        return count;
 
     }
 }
