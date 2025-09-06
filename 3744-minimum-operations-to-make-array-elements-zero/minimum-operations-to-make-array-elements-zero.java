3b0f9e927f238c2f39b9827b1e878de4eaaeb5f3
anavshukla2004 <anavshukla2504@gmail.com>
2025-09-06 22:32:42 +0530

    Time: 11 ms (100.00%) | Memory: 101.1 MB (58.33%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
@@ -1,30 +1,26 @@
 class Solution {
     public long minOperations(int[][] queries) {
-        List<Long> pow4 = new ArrayList<>();
-        pow4.add(1L);
-        while (pow4.get(pow4.size() - 1) <= 1_000_000_000L) {
-            pow4.add(pow4.get(pow4.size() - 1) * 4L);
-        }
-
-        List<Long> prefix = new ArrayList<>();
-        prefix.add(0L);
-        for (int i = 0; i < pow4.size() - 1; i++) {
-            prefix.add(prefix.get(prefix.size() - 1) + (long)(i + 1) * 3L * pow4.get(i));
-        }
-
         long res = 0;
-        for (int[] q : queries) {
-            int l = q[0], r = q[1];
-            long s = F(r, pow4, prefix) - F(l - 1, pow4, prefix);
-            res += (s + 1) / 2;
-        }
+        for (int[] query: queries) res += minOperations(query);
         return res;
     }
 
-    private long F(long n, List<Long> pow4, List<Long> prefix) {
-        if (n <= 0) return 0;
-        int k = Collections.binarySearch(pow4, n);
-        if (k < 0) k = -k - 2;
-        return prefix.get(k) + (long)(k + 1) * (n - pow4.get(k) + 1);
-    };
+    public static long minOperations(int[] query) {
+        long res0 = 0;
+        long x = 1;
+        while (x < query[0]) {
+            res0 ++;
+            x *= 4;
+        }
+        long res1 = res0;
+        long res = 0;
+        long prev = query[0];
+        while (x <= query[1] * 4L) {
+            res += res1 * (Math.min(x, query[1] + 1) - prev);
+            prev = x;
+            res1 ++;
+            x *= 4;
+        }
+        return (res + 1) / 2;
+    }
 }
