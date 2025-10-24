d8aac06bb6917d89a83d0a517d4bb6b4333e63b3
anavshukla2004 <anavshukla2504@gmail.com>
2025-10-24 23:48:08 +0530

    Time: 496 ms (16.15%) | Memory: 45.1 MB (7.69%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,22 +1,25 @@
+import java.util.HashMap;
+
 class Solution {
     public int nextBeautifulNumber(int n) {
-       while(true){
-        n=n+1;
-        if(isBeautiful(n))return n;
-       }
-        
-    }
-    public boolean isBeautiful(int n){
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        int temp=n;
-        while(temp>0){
-            int r=temp%10;
-            mp.put(r,mp.getOrDefault(r,0)+1);
-            temp/=10;
+        while (true) {
+            n++;
+            if (isBeautiful(n)) return n;
         }
-        for(int k:mp.keySet()){
-            if(k!=mp.get(k)){
-                 return false;
+    }
+
+    public boolean isBeautiful(int n) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        int temp = n;
+        while (temp > 0) {
+            int r = temp % 10;
+            mp.put(r, mp.getOrDefault(r, 0) + 1);
+            temp /= 10;
+        }
+        for (int k : mp.keySet()) {
+            if (k == 0 || mp.get(k) != k) {
+                return false;
             }
         }
         return true;
