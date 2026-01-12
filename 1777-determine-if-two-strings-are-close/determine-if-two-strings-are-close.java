878a72c2859258bb04be2586931ab7843cb3e4d3
Anav Shukla <anavshukla2504@gmail.com>
2026-01-12 22:43:48 +0530

    Time: 11 ms (83.71%) | Memory: 48 MB (41.59%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -14,8 +14,7 @@ class Solution {
             freq2[c - 'a']++;
         }
         for (int i = 0; i < 26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) ||
-                (freq1[i] != 0 && freq2[i] == 0)) {
+            if ((freq1[i] == 0) != (freq2[i] == 0)) {
                 return false;
             }
         }
